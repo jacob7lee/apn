60e30a2bb6057251229ecef75db328fa95a740b2
jacob7lee <jacob7lee@gmail.com>
2018-04-02 22:02:15 -0700

    branch 2

--- Property_Test.php
+++ Property_Test.php
@@ -11,7 +11,7 @@ Test
 include('Property.php');
 
 $obj = new Property ();
-$obj->setName ('My Property');
+$obj->setName ('My Property 2');
 $obj->price = 2500.00;
 $obj ['address_primary'] = '100 Main St';
 $obj->address_secondary = 'STE 100';
